cb2ed3004956d6c7a1e053de8a67f879bc000dba
Connor Peet <connor@peet.io>
2021-10-06 15:26:58 -0700

    fixups

--- src/failingDeepStrictEqualAssertFixer.ts
+++ src/failingDeepStrictEqualAssertFixer.ts
@@ -7,7 +7,6 @@ import {
   commands,
   Disposable,
   languages,
-  MarkdownString,
   Position,
   Range,
   TestMessage,
@@ -17,18 +16,21 @@ import {
   TextDocument,
   Uri,
   workspace,
-  WorkspaceEdit
+  WorkspaceEdit,
 } from 'vscode';
+import { memoizeLast } from './memoize';
 import { getTestMessageMetadata } from './metadata';
 
-const fixExpectedValueCommandId = 'selfhost-test.fix-test';
+const enum Constants {
+  FixCommandId = 'selfhost-test.fix-test',
+}
 
 export class FailingDeepStrictEqualAssertFixer {
   private disposables: Disposable[] = [];
 
   constructor() {
     this.disposables.push(
-      commands.registerCommand(fixExpectedValueCommandId, async (uri: Uri, position: Position) => {
+      commands.registerCommand(Constants.FixCommandId, async (uri: Uri, position: Position) => {
         const document = await workspace.openTextDocument(uri);
 
         const failingAssertion = detectFailingDeepStrictEqualAssertion(document, position);
@@ -45,18 +47,15 @@ export class FailingDeepStrictEqualAssertFixer {
         const end = document.positionAt(expectedValueNode.getEnd());
 
         const edit = new WorkspaceEdit();
-        edit.replace(uri, new Range(start, end), JSON.stringify(failingAssertion.actualJSONValue));
+        edit.replace(uri, new Range(start, end), formatJsonValue(failingAssertion.actualJSONValue));
         await workspace.applyEdit(edit);
       })
     );
 
     this.disposables.push(
       languages.registerCodeActionsProvider('typescript', {
-        provideCodeActions: async (document, range) => {
-          const failingAssertion = await detectFailingDeepStrictEqualAssertion(
-            document,
-            range.start
-          );
+        provideCodeActions: (document, range) => {
+          const failingAssertion = detectFailingDeepStrictEqualAssertion(document, range.start);
           if (!failingAssertion) {
             return undefined;
           }
@@ -64,7 +63,7 @@ export class FailingDeepStrictEqualAssertFixer {
           return [
             {
               title: 'Fix Expected Value',
-              command: fixExpectedValueCommandId,
+              command: Constants.FixCommandId,
               arguments: [document.uri, range.start],
             },
           ];
@@ -82,31 +81,61 @@ export class FailingDeepStrictEqualAssertFixer {
   }
 }
 
+const identifierLikeRe = /^[$a-z_][a-z0-9_$]*$/i;
+
+const tsPrinter = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+
+const formatJsonValue = (value: unknown) => {
+  if (typeof value !== 'object') {
+    return JSON.stringify(value);
+  }
+
+  const src = ts.createSourceFile('', `(${JSON.stringify(value)})`, ts.ScriptTarget.ES5, true);
+  const outerExpression = src.statements[0] as ts.ExpressionStatement;
+  const parenExpression = outerExpression.expression as ts.ParenthesizedExpression;
+
+  const unquoted = ts.transform(parenExpression, [
+    context => (node: ts.Node) => {
+      const visitor = (node: ts.Node): ts.Node =>
+        ts.isPropertyAssignment(node) &&
+        ts.isStringLiteralLike(node.name) &&
+        identifierLikeRe.test(node.name.text)
+          ? ts.factory.createPropertyAssignment(
+              ts.factory.createIdentifier(node.name.text),
+              node.initializer
+            )
+          : ts.visitEachChild(node, visitor, context);
+      return ts.visitNode(node, visitor);
+    },
+  ]);
+
+  return tsPrinter.printNode(ts.EmitHint.Expression, unquoted.transformed[0], src);
+};
+
+/** Parses the source file, memoizing the last document so cursor moves are efficient */
+const parseSourceFile = memoizeLast((text: string) =>
+  ts.createSourceFile('', text, ts.ScriptTarget.ES5, true)
+);
+
+const assertionFailureMessageRe = /^Expected values to be strictly (deep-)?equal:/;
+
+/** Gets information about the failing assertion at the poisition, if any. */
 function detectFailingDeepStrictEqualAssertion(
   document: TextDocument,
   position: Position
-): { assertion: DeepStrictEqualAssertion; actualJSONValue: unknown } | undefined {
-  const sf = ts.createSourceFile('', document.getText(), ts.ScriptTarget.ES5, true);
+): { assertion: StrictEqualAssertion; actualJSONValue: unknown } | undefined {
+  const sf = parseSourceFile(document.getText());
   const offset = document.offsetAt(position);
-  const assertion = DeepStrictEqualAssertion.atPosition(sf, offset);
+  const assertion = StrictEqualAssertion.atPosition(sf, offset);
   if (!assertion) {
     return undefined;
   }
 
   const startLine = document.positionAt(assertion.offsetStart).line;
-
   const messages = getAllTestStatusMessagesAt(document.uri, startLine);
-
-  function toString(value: string | MarkdownString): string {
-    if (typeof value === 'string') {
-      return value;
-    }
-    return value.value;
-  }
-
-  const strictDeepEqualMessage = messages.filter(m =>
-    toString(m.message).startsWith('Expected values to be strictly deep-equal:\n')
-  )[0];
+  const strictDeepEqualMessage = messages.find(m =>
+    assertionFailureMessageRe.test(typeof m.message === 'string' ? m.message : m.message.value)
+  );
 
   if (!strictDeepEqualMessage) {
     return undefined;
@@ -123,22 +152,31 @@ function detectFailingDeepStrictEqualAssertion(
   };
 }
 
-class DeepStrictEqualAssertion {
-  public static fromNode(node: ts.Node): DeepStrictEqualAssertion | undefined {
-    if (ts.isCallExpression(node) && node.expression.getText() === 'assert.deepStrictEqual') {
-      return new DeepStrictEqualAssertion(node);
+class StrictEqualAssertion {
+  /**
+   * Extracts the assertion at the current node, if it is one.
+   */
+  public static fromNode(node: ts.Node): StrictEqualAssertion | undefined {
+    if (!ts.isCallExpression(node)) {
+      return undefined;
     }
-    return undefined;
+
+    const expr = node.expression.getText();
+    if (expr !== 'assert.deepStrictEqual' && expr !== 'assert.strictEqual') {
+      return undefined;
+    }
+
+    return new StrictEqualAssertion(node);
   }
 
-  public static atPosition(
-    sf: ts.SourceFile,
-    offset: number
-  ): DeepStrictEqualAssertion | undefined {
+  /**
+   * Gets the equals assertion at the given offset in the file.
+   */
+  public static atPosition(sf: ts.SourceFile, offset: number): StrictEqualAssertion | undefined {
     let node = findNodeAt(sf, offset);
 
     while (node.parent) {
-      const obj = DeepStrictEqualAssertion.fromNode(node);
+      const obj = StrictEqualAssertion.fromNode(node);
       if (obj) {
         return obj;
       }
@@ -150,10 +188,12 @@ class DeepStrictEqualAssertion {
 
   constructor(private readonly expression: ts.CallExpression) {}
 
+  /** Gets the expected value */
   public get expectedValue(): ts.Expression | undefined {
     return this.expression.arguments[1];
   }
 
+  /** Gets the position of the assertion expression. */
   public get offsetStart(): number {
     return this.expression.getStart();
   }
