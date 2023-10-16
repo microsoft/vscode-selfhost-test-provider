1ceac96b25d079f12762c7f8b876ae8b11cff42a
Connor Peet <connor@peet.io>
2023-10-16 09:46:41 -0700

    adopt finalized api

--- src/snapshot.ts
+++ src/snapshot.ts
@@ -9,7 +9,7 @@ export const snapshotComment = '\n\n// Snapshot file: ';
 
 export const registerSnapshotUpdate = (ctrl: vscode.TestController) =>
   vscode.commands.registerCommand('selfhost-test-provider.updateSnapshot', async args => {
-    const message: vscode.TestMessage2 = args.message;
+    const message: vscode.TestMessage = args.message;
     const index = message.expectedOutput?.indexOf(snapshotComment);
     if (!message.expectedOutput || !message.actualOutput || !index || index === -1) {
       vscode.window.showErrorMessage('Could not find snapshot comment in message');
